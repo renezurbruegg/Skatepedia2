50bab40476b1f68b52aa20d389b176228af3123a
renezurbruegg <rene.zurbruegg@gmail.com>
2015-10-22 19:27:02 +0200

    Update SkatepediaFileClient.java

--- src/com/skatepedia/android/SkatepediaFileClient.java
+++ src/com/skatepedia/android/SkatepediaFileClient.java
@@ -5,14 +5,10 @@ import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
 public class SkatepediaFileClient extends FTPClient {
-    private final static char[] IP = { '4', '6', '.', '2', '5', '2', '.', '2',
-	    '2', '.', '7', '1' };
-    private final static char[] PASSWORD = { 's', 'k', 'a', 't', 'e', 'p', 'e',
-	    'd', 'i', 'a' };
+    private final static char[] IP = { /* Censored */ };
+    private final static char[] PASSWORD = { /* Censored */};
     private final static int PORT = 21;
-    private final static char[] USER = { '3', '3', '2', '9', '1', '1', '-',
-	    '2', '2', '9', '-', 's', 'k', 'a', 't', 'e', 'p', 'e', 'd', 'i',
-	    'a' };
+    private final static char[] USER = { /* Censored */};
 
     public SkatepediaFileClient() {
 	super();
